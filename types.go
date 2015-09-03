89bb73da421a046a23a8cb03d0ebd0abe54e0ba5
Jack Hayhurst <James.A.Hayhurst@gmail.com>
2015-09-03 15:37:38 -0400

    wrote the starts for interfaces for DNS records

--- types.go
+++ types.go
@@ -2,39 +2,31 @@ package main
 
 import ()
 
+
 type dnsRec struct {
-	TTL int
-	value string
-	String() string
-}
-
-type aRec struct {
-	dnsRec
-}
-
-type cnameRec struct {
-	dnsRec
-}
-
-type mxRec struct {
-	dnsRec
-	priority int
-}
-
-type txtRec struct {
-	dnsRec
-}
-
-type nsRec struct {
-	cnameRec
-}
-
-type ptrRec struct {
-	aRec
+	name string
+	ttl int
+	recType string
+	priority int //priority
+	protocol string
+	service string
+	weight string
+	port string
+	comment string
+	value string // also commonly called target
 }
 
 type soaRec struct {
-
+	name string
+	ttl int
+	const recTyep := "SOA"
+	primary string
+	contact string
+	serial int
+	refresh int
+	retry int
+	expire int
+	minimum int
 }
 
 type fqdn struct {
@@ -48,4 +40,41 @@ type zone struct {
 	soa soaRec
 	defaultTTL int
 	tld fqdn
+}
+
+// Returns the DNS record in standard zone file format.
+func (dnsRec) String() (string) {
+	if dnsRec.ttl == 0 {
+		switch dnsRec.recType {
+		case "A", "AAAA", "NS", "CNAME":
+			return strings.Join(
+				[]string{
+					dnsRec.name,
+					"IN",
+					dnsRec.type,
+					dnsRec.value
+				}, "\t")
+		case "MX":
+			return strings.Join(
+				[]string{
+					dnsRec.name,
+					"IN",
+					dnsRec.type,
+					dnsRec.priority,
+					dnsRec.value
+				}, "\t")
+		case "TXT":
+			return strings.Join(
+				[]string{
+					dnsRec.name,
+					"IN",
+					dnsRec.type,
+					'"' + dnsRec.value + '"'
+				}, "\t")
+		}
+	}
+}
+
+func (*zone) isRec(input []byte) err {
+
 }
